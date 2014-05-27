42628f08add1f7fd3ea1f9530f1bf6833995016a
Ha Truong <truongvietha87@gmail.com>
2014-05-27 22:56:07 +0700

    change to use all slashs

--- dsv_script_template.js
+++ dsv_script_template.js
@@ -1,22 +1,22 @@
 //
 // ============================================================================
-// Copyright: 
-//          This source is subject to the Designveloper JSC (designveloper.com) 
+// Copyright:
+//          This source is subject to the Designveloper JSC (designveloper.com)
 //          All using or modify must have permission from us.
 //
 // Name:    DSVScriptTemplate
-// Purpose: 	
+// Purpose:
 //          Describe the purpose of the script [short version]
-// Class:        
+// Class:
 //          one ; two ; three
-// Functions:        
+// Functions:
 //          one ; two ; three
-// Called From:       
+// Called From:
 //          (script) any
-// Author:            
+// Author:
 //          Ha Truong (truongvieth@designveloper.com)
-// Notes:             
-//          Additional information [long version]  
+// Notes:
+//          Additional information [long version]
 // Changelog:
 //          05/27/2014 - Ha Truong - Init first revision.
 // ============================================================================
@@ -30,7 +30,7 @@
 //  Use slashes for comments
 
 
-//	Use single quotes: 
+//	Use single quotes:
 var foo = 'bar';
 
 // Declare one variable per var statement
@@ -47,7 +47,7 @@ if (true) {
   console.log('winning');
 }
 
-// Object or Array creation: Use trailing commas and put short declarations 
+// Object or Array creation: Use trailing commas and put short declarations
 // on a single line. Only quote keys when your interpreter complains:
 var a = ['hello', 'world'];
 var b = {
@@ -65,80 +65,81 @@ if (a !== '') {
 var foo = (a === b)
   ? 1
   : 2;
-  
-// Name your closures  
+
+// Name your closures
 req.on('end', function onEnd() {
   console.log('winning');
 });
-  
+
 
 // Use UPPERCASE for Constants
-var SECOND = 1 * 1000;
+var SECOND = 1// 1000;
 
 // Use lowerCamelCase for variables, properties and function names
-var adminUser = db.query('SELECT * FROM users ...');
+var adminUser = db.query('SELECT// FROM users ...');
 
-/**
- * -------------------------------------------------------------------
- * customFunctionName ( parameter1 ; parameter2 )
- *
- * PARAMETERS:
- *            @parameter1 (text) Input string
- *            @parameter2 (num) Numerical value
- * RETURNS:
- *            (bool) True or False based on proper
- *            evaluation
- * DEPENDENCIES:
- *            none
- * PURPOSE:
- *            Use this function in order to accomplish
- *            most wonderful things possible!
- * NOTES:
- *            none
- * REVISIONS:
- *            05/23/14 - Initial release
- *            05/25/14 - Modified to fix issue
- *            05/27/14 - No returning Boolean result
- * -------------------------------------------------------------------
- */
+
+
+// -------------------------------------------------------------------
+// customFunctionName ( parameter1 ; parameter2 )
+//
+// PARAMETERS:
+//            @parameter1 (text) Input string
+//            @parameter2 (num) Numerical value
+// RETURNS:
+//            (bool) True or False based on proper
+//            evaluation
+// DEPENDENCIES:
+//            none
+// PURPOSE:
+//            Use this function in order to accomplish
+//            most wonderful things possible!
+// NOTES:
+//            none
+// REVISIONS:
+//            05/23/14 - Initial release
+//            05/25/14 - Modified to fix issue
+//            05/27/14 - No returning Boolean result
+// -------------------------------------------------------------------
+
 function customFunctionName(request, response) {
 
   response.writeHead(200, {
 	'Content-Type':'text/html'
   });
-  
+
   var rs = fs.createReadStream(__dirname + '/template.html');
   sys.pump(rs, response);
   return true;
-};  
-  
- 
+};
+
+
 // Use UpperCamelCase for class names
-  
-/**
- * -------------------------------------------------------------------
- * CustomClassName ( parameter1 ; parameter2 )
- *
- * PARAMETERS:
- *            @parameter1 (text) Input string
- *            @parameter2 (num) Numerical value
- * METHODS:
- *            one
- *            two
- * DEPENDENCIES:
- *            none
- * PURPOSE:
- *            Use this function in order to accomplish
- *            most wonderful things possible!
- * NOTES:
- *            none
- * REVISIONS:
- *            05/23/14 - Initial release
- *            05/27/14 - Modified to fix issue
- *            05/31/10 - No returning Boolean result
- * -------------------------------------------------------------------
- */
- // Constructor
+
+
+// -------------------------------------------------------------------
+// CustomClassName ( parameter1 ; parameter2 )
+//
+// PARAMETERS:
+//            @parameter1 (text) Input string
+//            @parameter2 (num) Numerical value
+// METHODS:
+//            one
+//            two
+// DEPENDENCIES:
+//            none
+// PURPOSE:
+//            Use this function in order to accomplish
+//            most wonderful things possible!
+// NOTES:
+//            none
+// REVISIONS:
+//            05/23/14 - Initial release
+//            05/27/14 - Modified to fix issue
+//            05/31/10 - No returning Boolean result
+// -------------------------------------------------------------------
+
+// Constructor
 function CustomClassName(bar,foo) {
   // always initialize all instance properties
   this.bar = bar;
@@ -150,6 +151,3 @@ CustomClassName.prototype.fooBar = function() {
 };
 // export the class
 module.exports = CustomClassName;
- 
- 
-
